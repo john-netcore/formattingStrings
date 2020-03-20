0a84f68bd09c849cc5c54b6f2ee8f65bcead77e4
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-03-20 11:21:55 +0100

    First commit

---
+++ src/FormattingStrings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FormattingStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
